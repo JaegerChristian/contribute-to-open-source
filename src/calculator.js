fc4caae58a6926a748833d6ab902c4b31f2b377e
Charulekha <charulekhapandian@gmail.com>
2024-12-27 11:04:24 +0530

    Implement _check

--- src/calculator.js
+++ src/calculator.js
@@ -1,29 +1,28 @@
-exports._check = function(a,b) {
+exports._check = (a, b) => {
   if (typeof a !== 'number' || typeof b !== 'number') {
     throw new TypeError('Arguments must be numbers');
   }
 };
 
-exports.add = function (a, b) {
+exports.add = (a, b) => {
   exports._check(a, b);
   return a + b;
 };
 
-exports.subtract = function (a, b) {
+exports.subtract = (a, b) => {
   exports._check(a, b);
   return a - b;
 };
 
-exports.multiply = function (a, b) {
+exports.multiply = (a, b) => {
   exports._check(a, b);
   return a * b;
 };
 
-exports.divide = function (a, b) {
+exports.divide = (a, b) => {
   exports._check(a, b);
   if (b === 0) throw new Error('Cannot divide by zero');
   return a / b;
 };
-
 
 module.exports = exports;
