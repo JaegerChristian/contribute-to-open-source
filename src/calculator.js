cbcef84fc6a507e6b6d9303ce524eef90900e9bd
marshallrussell <marshallrussell.x@gmail.com>
2025-03-09 20:25:33 -0500

    Revert calculator.js
    The Copilot saw a solution, but it was incomplete.

--- src/calculator.js
+++ src/calculator.js
@@ -6,23 +6,41 @@ exports._check = () => {
 };
 
 exports.add = (x, y) => {
-  exports._check(x,y);
+  if (typeof x !== 'number') {
+    throw new TypeError(`${x} is not a number`);
+  }
+  if (typeof y !== 'number') {
+    throw new TypeError(`${y} is not a number`);
+  }
   return x + y;
 };
 
 exports.subtract = (x, y) => {
-  exports._check(x,y);
+  if (typeof x !== 'number') {
+    throw new TypeError(`${x} is not a number`);
+  }
+  if (typeof y !== 'number') {
+    throw new TypeError(`${y} is not a number`);
+  }
   return x - y;
 };
 
 exports.multiply = (x, y) => {
-  exports._check(x,y);
+  if (typeof x !== 'number') {
+    throw new TypeError(`${x} is not a number`);
+  }
+  if (typeof y !== 'number') {
+    throw new TypeError(`${y} is not a number`);
+  }
   return x * y;
 };
 
 exports.divide = (x, y) => {
-  exports._check(x,y);
+  if (typeof x !== 'number') {
+    throw new TypeError(`${x} is not a number`);
+  }
+  if (typeof y !== 'number') {
+    throw new TypeError(`${y} is not a number`);
+  }
   return x / y;
 };
-
-module.exports = exports;
