68cba5f22cb8c42d882b0e7140b98b9cf9de0033
Christian Schröer <schroeer-christian@web.de>
2025-05-04 10:45:12 +0200

    bugg fix
    (dryer code and 0 can´t be devided)

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -1,10 +1,13 @@
 /* eslint-disable no-unused-expressions */
 const calculator = require('./calculator');
 
-describe.skip('_check', () => {
+
+
+describe.only('_check', () => {
   beforeEach(() => {
     sinon.spy(calculator, '_check');
   });
+
 
   afterEach(() => {
     calculator._check.restore();
